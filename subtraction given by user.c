91c1c65b5605e76f0191fffcd1813877cbe9b54e
sivanisingh2026 <Sivani221@outlook.com>
2026-08-02 10:01:36 +0530

    Add subtraction program in C
    4th program given by user

---
+++ subtraction given by user.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b,sub;
+printf("enter the first no: ");
+scanf("%d",&a);
+printf("enter the second no: ");
+scanf("%d",&b);
+sub = a - b;
+printf("sub =%d\n",sub);
+return 0;
+}
